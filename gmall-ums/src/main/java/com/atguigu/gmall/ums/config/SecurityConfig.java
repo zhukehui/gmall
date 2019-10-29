b5c6623ee3f1d1a39199827f2b6c9611ed2e28d8
kehui <574059694@qq.com>
2019-10-29 22:35:14 +0800

    谷粒商城环境初步搭建完成

---
+++ gmall-ums/src/main/java/com/atguigu/gmall/ums/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.atguigu.gmall.ums.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author eternity
+ * @create 2019-10-29 20:33
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();//开放所有权限
+        http.csrf().disable();
+    }
+}
--- gmall-wms/src/main/java/com/atguigu/gmall/wms/service/FeightTemplateService.java
+++ gmall-wms/src/main/java/com/atguigu/gmall/wms/service/FeightTemplateService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.atguigu.gmall.wms.entity.FeightTemplateEntity;
 import com.atguigu.core.bean.PageVo;
 import com.atguigu.core.bean.QueryCondition;
+import org.springframework.stereotype.Service;
 
 
 /**
@@ -13,6 +14,7 @@ import com.atguigu.core.bean.QueryCondition;
  * @email 574059694@qq.com
  * @date 2019-10-28 23:06:04
  */
+@Service
 public interface FeightTemplateService extends IService<FeightTemplateEntity> {
 
     PageVo queryPage(QueryCondition params);
